864fbfd3f1cf168591da7a424ab9890ed9726d02
vilanz <vs.lanzarini@gmail.com>
2021-06-14 23:04:04 -0300

    renaming file

--- release-assets/create-assets.sh
+++ release-assets/create-assets.sh
@@ -1,11 +1,12 @@
 set -e
 
-cd "${0%/*}"
+# set pwd as /release-assets
+cd "${0%/*}" 
 
 echo "1. Creatimg meta.json"
 
 HASH_OUTPUT_PREFIX="BLT folder hash: "
-HASH=$(./hash.exe ../src | sed "s/$HASH_OUTPUT_PREFIX//g")
+HASH=$(./blt-hasher.exe ../src | sed "s/$HASH_OUTPUT_PREFIX//g")
 sed "s/__hash__/$HASH/g" base.meta.json > meta.json
 
 echo "2. Zipping game files to XpNotifier.zip"
