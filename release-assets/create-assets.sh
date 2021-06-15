45027b32a07695a4e0f586bfca2fe2cd7a400f01
vilanz <vs.lanzarini@gmail.com>
2021-06-14 23:28:47 -0300

    make ZIP's root folder be /XpNotifier

--- release-assets/create-assets.sh
+++ release-assets/create-assets.sh
@@ -5,11 +5,21 @@ cd "${0%/*}"
 
 echo "1. Creatimg meta.json"
 
+echo "Running BLT Hasher..."
 HASH_OUTPUT_PREFIX="BLT folder hash: "
 HASH=$(./blt-hasher.exe ../src | sed "s/$HASH_OUTPUT_PREFIX//g")
+
+echo "Creating meta.json with hash $HASH..."
 sed "s/__hash__/$HASH/g" base.meta.json > meta.json
 
 echo "2. Zipping game files to XpNotifier.zip"
 
-echo "Zipping src folder"
-./7za.exe a XpNotifier.zip ../src
+echo "Creating XpNotifier folder..."
+mkdir -p XpNotifier
+rm -f XpNotifier/*
+cp ../src/* ./XpNotifier
+
+echo "Zipping src folder..."
+./7za.exe a XpNotifier.zip ./XpNotifier/ > /dev/null
+
+echo "Assets created!"
