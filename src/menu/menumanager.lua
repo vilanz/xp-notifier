9a3eb054d03ab0c9c43e18f4ebda71e1c9ab33bf
Buurazu <excel_saga_rules@hotmail.com>
2021-08-27 05:42:37 -0400

    Add files via upload

---
+++ src/menu/menumanager.lua
@@ -0,0 +1,22 @@
+local key = ModPath .. '	' .. RequiredScript
+if _G[key] then return else _G[key] = true end
+
+Hooks:Add('LocalizationManagerPostInit', 'LocalizationManagerPostInit_XPNotifier', function(loc)
+	loc:load_localization_file(XPNotifier._path .. 'menu/english.txt', false)
+end)
+
+Hooks:Add('MenuManagerInitialize', 'MenuManagerInitialize_XPNotifier', function(menu_manager)
+
+	MenuCallbackHandler.XPNotifierOptionsCheckbox = function(this, item)
+		XPNotifier.settings[item:name()] = item:value() == 'on'
+	end
+	
+	MenuCallbackHandler.XPNotifierOptionsSave = function(this, item)
+		XPNotifier:Save()
+	end
+
+	XPNotifier:Load()
+
+	MenuHelper:LoadFromJsonFile(XPNotifier._path .. 'menu/options.txt', XPNotifier, XPNotifier.settings)
+
+end)
